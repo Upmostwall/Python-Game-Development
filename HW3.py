f9cd37d8d45a2cb9d48ee087797de9641ddb12f6
Upmostwall <zhengz17@outlook.com>
2026-02-08 19:08:50 +0000

    Add files via upload

---
+++ HW3.py
@@ -0,0 +1,18 @@
+matrix = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16]
+]
+
+print("Matrix:")
+for row in matrix:
+    print(row)
+
+rows = len(matrix)
+columns = len(matrix[0])
+
+print("\nNumber of rows: {}".format(rows))
+print("Number of columns: {}".format(columns))
+
+
